80f40a4269ebe5f3c438529a32c5bd8172b99ae6
Alexis Peralta <AlPeralta@eagle.org>
2023-09-13 07:35:56 -0500

    anime girl

--- js/ArScene.tsx
+++ js/ArScene.tsx
@@ -1,28 +1,39 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import {
+  Viro3DObject,
+  ViroAmbientLight,
   ViroARScene,
+  ViroNode,
   ViroText,
 } from '@viro-community/react-viro';
 export default class ARScene extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
-    // Set initial state here
     this.state = {
       text: 'Initializing AR...',
     };
-    // bind 'this' to functions
     this._onInitialized = this._onInitialized.bind(this);
   }
   render() {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized}>
-        <ViroText
-          text={this.state.text}
-          scale={[0.5, 0.5, 0.5]}
-          position={[0, 0, -1]}
-          style={styles.helloWorldTextStyle}
-        />
+          <ViroAmbientLight color="#ffffff" />
+          <ViroNode position={[0, -1, -1]} dragType="FixedToWorld" onDrag={() => {}}>
+            <Viro3DObject
+              source={require('../assets/hip_hop/hip_hop_anime_girl.vrx')}
+              resources={[
+                require('../assets/hip_hop/Atlas_75340.png'),
+                require('../assets/hip_hop/body_texture.png'),
+                require('../assets/hip_hop/F00_000_Face_00.png'),
+              ]}
+              position={[0, -20, -50]}
+              scale={[0.001, 0.001, 0.001]}
+              rotation={[90, 0, 0]}
+              type="VRX"
+              animation={{ run: true, loop: true }}
+            />
+          </ViroNode>
       </ViroARScene>
     );
   }
